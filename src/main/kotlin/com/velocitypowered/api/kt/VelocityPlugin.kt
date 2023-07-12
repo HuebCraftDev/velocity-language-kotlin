0c4061214cba43175e00333e997883420a003458
silenium-dev <me@silenium-dev.net>
2023-07-12 14:10:02 +0200

    Add "The HuebCraft Team" to authors

--- src/main/kotlin/com/velocitypowered/api/kt/VelocityPlugin.kt
+++ src/main/kotlin/com/velocitypowered/api/kt/VelocityPlugin.kt
@@ -11,7 +11,7 @@ import org.slf4j.Logger
 
 @Plugin(
     id = "velocity-language-kotlin",
-    authors = ["Velocity Contributors"],
+    authors = ["Velocity Contributors", "The HuebCraft Team"],
     version = BuildConstants.BUILD_VERSION
 )
 @Suppress("unused")
