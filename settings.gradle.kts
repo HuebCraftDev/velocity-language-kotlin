10bfc84b3281933861c5df46d0c53c7cdf1e56be
Silas Della Contrada <me@silenium-dev.net>
2026-05-09 21:27:06 +0200

    chore: update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ pluginManagement {
         kotlin("kapt") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
 
-        id("com.gradleup.shadow") version "9.3.0"
+        id("com.gradleup.shadow") version "9.4.1"
+        id("org.jetbrains.gradle.plugin.idea-ext") version "1.4.1"
     }
 }
